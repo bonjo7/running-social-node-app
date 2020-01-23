7b9c1b132873805b8c8fd1719a08615b36d3376f
Bernard Thompson <bethomps@redhat.com>
2020-01-23 21:57:01 +0000

    Created mongoDB connection

--- application.js
+++ application.js
@@ -1,9 +1,24 @@
 const express = require('express');
+const DB = require('./db/db');
 
+const dotenv = require('dotenv');
+dotenv.config();
+
+//Initialize app variable with express
 const app = express();
 
-const port = process.env.PORT || 3001;
+//Set port value for app to run on if env var is unavailable default to 3001
+const PORT = process.env.PORT || 3001;
+//Define enviornment from env var, default dev
+const ENV = process.env.APP_ENV || 'dev';
 
+DB();
+
+//Get request to send data to the browser
 app.get('/', (req, res) => res.send('Node application is running successfully'));
 
-app.listen(port, () => console.log('App started at: ' + new Date() + ' on port: ' + port));
+//Pass in port with function call back 
+app.listen(PORT, () => console.log('App started at: ' + new Date() 
+    + '\nOn port: ' 
+    + PORT + '.\nEnvoirnment: ' 
+    + ENV ));
