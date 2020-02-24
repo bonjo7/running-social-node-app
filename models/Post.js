4145038211292e4683319cad2a5b8d1ed953373e
Bernard Thompson <bethomps@redhat.com>
2020-02-24 20:41:36 +0000

    User can not give a positive to a post

--- models/Post.js
+++ models/Post.js
@@ -16,7 +16,7 @@ const PostSchema = new mongoose.Schema({
         type: Date,
         default: Date.now
     },
-    likes: [{
+    positives: [{
         user: {
             type: mongoose.Schema.Types.ObjectId,
             ref: 'user'
