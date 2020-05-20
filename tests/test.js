1d3c9295f9ec4166e4cb57093381ab4c978dbbbb
Bernard Thompson <bethomps@redhat.com>
2020-05-20 16:24:59 +0100

    Adding tests

---
+++ tests/test.js
@@ -0,0 +1,41 @@
+const dotenv = require("dotenv");
+const request = require('supertest');
+const app = require("../application.js");
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+
+dotenv.config();
+const ENV = process.env.APP_ENV || "dev";
+
+const { expect } = chai;
+chai.use(chaiHttp);
+
+describe("Server!", () => {
+  it("Node application is running successfully", (done) => {
+    chai
+      .request(app)
+      .get("/")
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body.status).to.equals("Application Launched");
+        expect(res.body.message).to.equals(
+          "Node application is running successfully" + ENV
+        );
+        done();
+      });
+  });
+
+  describe('Login API', function() {
+    it('Should success if credential is valid', function(done) {
+        request(app)
+           .post('/lib/routes/auth')
+           .set('Accept', 'application/json')
+           .set('Content-Type', 'application/json')
+           .send({ username: 'username', password: 'password' })
+           .expect(200)
+          
+           .end(done);
+    }); 
+});
+
+});
